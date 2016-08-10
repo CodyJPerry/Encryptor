cb9459033efcfbe931434479642431bd0ada8a57
Cody Perry <codyperry65@yahoo.com>
2016-08-09 23:53:26 -0400

    Refactor cipher method

--- encryptor.rb
+++ encryptor.rb
@@ -1,24 +1,23 @@
 class Encryptor
-	def cipher 
-		{ 'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q', 'e' => 'r', 'f' => 's', 'g' => 't',
-		  'h' => 'u', 'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y', 'm' => 'z', 'n' => 'a',
-		  'o' => 'b', 'p' => 'c', 'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g', 'u' => 'h',
-		  'v' => 'i', 'w' => 'j', 'x' => 'k', 'y' => 'l', 'z' => 'm'
-		 }
+	def cipher(rotation) 
+		characters = (' '..'z').to_a
+		rotated_characters = characters.rotate(rotation)
+		Hash[characters.zip(rotated_characters)]
 	end
 
-	def encrypt_letter(letter) 
-		lowercase_letter = letter.downcase
-		cipher[lowercase_letter]
+	def encrypt_letter(letter, rotation) 
+		rotation = 13
+		cipher_for_rotation = cipher(rotation)
+		cipher_for_rotation[letter]
 	end
 
-	def encrypt(string)
+	def encrypt(string, rotation)
 		#1. Cut the string into letters
 		letters = string.split("")
 		#2. Encrypt those letters one at a time
 		results = []
 		letters.collect do |letter| 
-			encrypted_letter = results.push(encrypt_letter(letter))
+			encrypted_letter = results.push(encrypt_letter(letter, rotation))
 			
 		end
 		#3. Join the results back together into one string
